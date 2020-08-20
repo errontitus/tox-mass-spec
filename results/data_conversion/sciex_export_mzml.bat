7f0330659d9297b4d1c744e7e817aaf5c52a1be6
Erron Titus <errontitus@gmail.com>
2020-08-20 00:13:57 -0700

    .

---
+++ results/data_conversion/sciex_export_mzml.bat
@@ -0,0 +1,5 @@
+cd "C:\Program Files\AB SCIEX\MS Data Converter"
+
+AB_SCIEX_MS_Converter WIFF "D:\Analyst Data\Projects\QTOF_CDS\Data\QTOF_CDS_2020_08_18.wiff" -profile MZML "C:\Users\How\Documents\Erron\QTOF_CDS_2020_08_18.mgf"
+
+Pause
